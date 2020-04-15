0acadee9cc91611aaa2fa4a94c82ac2f3f433492
Tsakos Kostas <ktsakos@isc.tuc.gr>
2020-04-15 17:18:27 +0000

    A testing php file

--- www/index.php
+++ www/index.php
@@ -15,32 +15,7 @@
     <div class="container">
     <?php echo "<h1>Hi! I'm happy</h1>"; ?>
 
-    <?php
-
-    $conn = mysqli_connect('db', 'user', 'test', "myDb");
-
-
-    $query = 'SELECT * From Person';
-    $result = mysqli_query($conn, $query);
-
-    echo '<table class="table table-striped">';
-    echo '<thead><tr><th></th><th>id</th><th>name</th></tr></thead>';
-    while($value = $result->fetch_array(MYSQLI_ASSOC)){
-        echo '<tr>';
-        echo '<td><a href="#"><span class="glyphicon glyphicon-search"></span></a></td>';
-        foreach($value as $element){
-            echo '<td>' . $element . '</td>';
-        }
-
-        echo '</tr>';
-    }
-    echo '</table>';
-
-    $result->close();
-
-    mysqli_close($conn);
-
-    ?>
+ 
     </div>
 </body>
 </html>
